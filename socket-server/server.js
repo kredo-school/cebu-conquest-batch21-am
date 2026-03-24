41671dd031a34048172f018451f3236636dd7d20
KeitaIshimatsu <keita.imo.0928@gmail.com>
2026-03-24 09:47:13 +0800

    env作成

--- socket-server/server.js
+++ socket-server/server.js
@@ -11,7 +11,7 @@ const PORT = 3001;
 // CORS設定: クライアント側(5173等)からのアクセスを許可
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:8000"],
+        origin: ["*"],
         methods: ["GET", "POST"]
     }
 });
